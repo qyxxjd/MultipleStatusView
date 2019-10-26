a0fb44fe8f2646624635fc57832279ae3b5fbdf9
xiaohuigao <xiaohuigao@pateo.com.cn>
2019-10-26 16:24:27 +0800

    add attach methods.

--- app/src/main/java/com/classic/common/simple/Simple3Activity.java
+++ app/src/main/java/com/classic/common/simple/Simple3Activity.java
@@ -11,13 +11,10 @@ public class Simple3Activity extends AbsActivity {
         setContentView(R.layout.activity_simple3);
         setTitle("Simple3Activity");
 
-//        mMultipleStatusView = findViewById(R.id.simple2_multiple_status_view);
-//        mMultipleStatusView.setOnRetryClickListener(mRetryClickListener);
-
         ViewGroup ll_content = findViewById(R.id.ll_content);
         mMultipleStatusView = MultipleStatusView.attach(ll_content);
-        mMultipleStatusView.setOnRetryClickListener(mRetryClickListener);
-
-        loading();
+        if (null != mMultipleStatusView) {
+            mMultipleStatusView.setOnRetryClickListener(mRetryClickListener);
+        }
     }
 }
